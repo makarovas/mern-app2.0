c0f33c03efb04266911b08e9135ef6650080f449
Alexander Makarof <makarofalex@gmail.com>
2020-03-24 22:59:55 +0300

    start front

--- frontend/src/components/NewGoal/NewGoal.jsx
+++ frontend/src/components/NewGoal/NewGoal.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeid } from "../fakes/users";
 export default function NewGoal(props) {
+  const [inputValue, setInputValue] = useState("");
   const addGoalHandler = e => {
     e.preventDefault();
     const newGoal = {
       id: Math.floor(Math.random() * 109),
-      text: makeid(213)
+      text: inputValue
     };
     props.onAddGoal(newGoal);
+    setInputValue("");
   };
+
+  const textChangeHandler = e => setInputValue(e.target.value);
   return (
     <div>
       <form onSubmit={addGoalHandler}>
-        <input type="text" name="goal" />
+        <input
+          type="text"
+          name="goal"
+          onChange={textChangeHandler}
+          value={inputValue}
+        />
         <button type="submit">add</button>
       </form>
     </div>
