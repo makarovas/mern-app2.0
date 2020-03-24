91efb98eef4c65a543df2a57376183dedc060b38
Alexander Makarof <makarofalex@gmail.com>
2020-03-24 22:02:37 +0300

    start front

--- frontend/src/components/NewGoal/NewGoal.jsx
+++ frontend/src/components/NewGoal/NewGoal.jsx
@@ -1,16 +1,6 @@
 import React from "react";
-
+import { makeid } from "../fakes/users";
 export default function NewGoal(props) {
-  function makeid(length) {
-    var result = "";
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
   const addGoalHandler = e => {
     e.preventDefault();
     const newGoal = {
